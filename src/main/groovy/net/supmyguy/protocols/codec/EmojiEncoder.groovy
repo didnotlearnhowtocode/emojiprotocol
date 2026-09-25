6ae25d695c5153f5677b07542969d9020b9a53b1
ParaElDinero <ask@mateusz.cz>
2026-09-25 15:52:03 +0200

    emoji encoder v1 implementation fix

--- src/main/groovy/net/supmyguy/protocols/codec/EmojiEncoder.groovy
+++ src/main/groovy/net/supmyguy/protocols/codec/EmojiEncoder.groovy
@@ -2,23 +2,25 @@ package net.supmyguy.protocols.codec
 
 import net.supmyguy.protocols.domain.alphabet.EmojiAlphabet
 
+import java.nio.charset.StandardCharsets
+
 class EmojiEncoder {
 
-    String encode(byte[] messageBytes) {
-        return convertIntoSextets(messageBytes).collect { String it -> EmojiAlphabet.findEmojiByBits(it) }.join("")
+    String encode(String decoded) {
+        return convertIntoEmojiBitGroups(decoded.getBytes(StandardCharsets.UTF_8)).collect { String it -> EmojiAlphabet.findEmojiByBits(it) }.join("")
     }
 
-    private List<String> convertIntoSextets(byte[] messageBytes) {
-        List<String> sextets = []
+    private List<String> convertIntoEmojiBitGroups(byte[] messageBytes) {
+        List<String> groups = []
         String bitsPadding = "0".repeat(6 - (messageBytes.length * 8) % 6)
         String formattedMessageBitsSequence = messageBytes.collect { Byte b -> formatIntoUnsignedByte(b) }.join("") + bitsPadding
-        int sextetsCount = formattedMessageBitsSequence.length() / 6
-        (0..<sextetsCount).each { Integer iSextet ->
+        int groupsCount = formattedMessageBitsSequence.length() / 6
+        (0..<groupsCount).each { Integer iSextet ->
             int leftmost = iSextet * 6
             int rightmost = leftmost + 6
-            sextets << formattedMessageBitsSequence.substring(leftmost, rightmost)
+            groups << formattedMessageBitsSequence.substring(leftmost, rightmost)
         }
-        return sextets
+        return groups
     }
 
     private String formatIntoUnsignedByte(byte b) {
