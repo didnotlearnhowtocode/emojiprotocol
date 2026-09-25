346f36290d76d2a9db6313121c222e484f277c8e
ParaElDinero <ask@mateusz.cz>
2026-09-25 15:52:46 +0200

    emoji decoder v1 implementation

---
+++ src/main/groovy/net/supmyguy/protocols/codec/EmojiDecoder.groovy
@@ -0,0 +1,22 @@
+package net.supmyguy.protocols.codec
+
+import net.supmyguy.protocols.domain.alphabet.EmojiAlphabet
+
+class EmojiDecoder {
+
+    String decode(String encoded) {
+        List<String> emojis = encoded.codePoints().collect { Integer it -> String.valueOf(Character.toChars(it)) }
+        String combinedEmojisBitsSequence = emojis.collect { String it -> EmojiAlphabet.findEmojiBitsSequence(it) }.join("")
+        int bitPaddingStart = combinedEmojisBitsSequence.length() - (combinedEmojisBitsSequence.length() % 8)
+        String reducedEmojiBitsSequence = combinedEmojisBitsSequence.substring(0, bitPaddingStart)
+        int decodedByteGroups = reducedEmojiBitsSequence.length() / 8
+        String decoded = (0..<decodedByteGroups).collect { Integer groupId ->
+            int leftmost = groupId * 8
+            int rightmost = leftmost + 8
+            String bits = reducedEmojiBitsSequence.substring(leftmost, rightmost)
+            return "${(char) Integer.parseInt(bits, 2)}"
+        }.join("")
+        println("Decoded message: $decoded")
+        return ""
+    }
+}
